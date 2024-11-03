3aa7e0987b3531e7f8b032cc6f9dc9225fac3b64
akoziol <arkadiuszkoziol2@gmail.com>
2024-11-03 22:03:27 +0100

    material repository interface

--- backend/app/Interfaces/Repositories/MaterialRepositoryInterface.php
+++ backend/app/Interfaces/Repositories/MaterialRepositoryInterface.php
@@ -2,10 +2,15 @@
 
 namespace App\Interfaces\Repositories;
 
-use App\Dto\ToolDTO;
-use App\Models\Tool;
+use App\Dto\MaterialDto;
+use App\Models\Material;
 
 interface MaterialRepositoryInterface
 {
+    public function create(MaterialDto $materialDto): Material;
+    public function update(Material $material, MaterialDto $materialDto): bool;
+    public function delete(Material $material): bool;
+    public function findById(int $id): ?Material;
+    public function findAll(): array;
 }
 
