5251cbddd1c39aefdc591ecdc61c2b190f779ed2
akoziol <arkadiuszkoziol2@gmail.com>
2024-11-04 17:42:01 +0100

    laravel.log at start

--- .docker/php/start.sh
+++ .docker/php/start.sh
@@ -10,6 +10,8 @@ php artisan route:clear &&
 php artisan view:clear &&
 php artisan storage:link &&
 php artisan l5-swagger:generate &&
+mkdir -p /var/www/app/storage/logs &&
+touch /var/www/app/storage/logs/laravel.log &&
 chmod 755 -R storage &&
 
 if ! pgrep php-fpm > /dev/null 2>&1; then
