28c9a9e75a003a3fca448e343ba84cb9b07071a4
akoziol <arkadiuszkoziol2@gmail.com>
2024-11-04 17:30:53 +0100

    fix docker

--- .docker/php/start.sh
+++ .docker/php/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-php-fpm &&
 composer install &&
 php artisan key:generate &&
 php artisan migrate &&
@@ -12,3 +11,7 @@ php artisan view:clear &&
 php artisan db:seed &&
 php artisan storage:link &&
 php artisan l5-swagger:generate
+
+if ! pgrep php-fpm > /dev/null 2>&1; then
+  php-fpm
+fi
