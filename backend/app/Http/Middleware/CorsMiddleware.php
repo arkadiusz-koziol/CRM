3838a3506b9d1e33f6a509cfe8f9750924678727
akoziol <arkadiuszkoziol2@gmail.com>
2025-09-05 20:02:33 +0200

    psr12

--- backend/app/Http/Middleware/CorsMiddleware.php
+++ backend/app/Http/Middleware/CorsMiddleware.php
@@ -22,8 +22,6 @@ class CorsMiddleware
             'http://127.0.0.1:3000',
             'http://localhost:5173',
             'http://127.0.0.1:5173',
-            'http://localhost:3001',
-            'http://127.0.0.1:3001',
         ];
 
         $origin = $request->header('Origin');
