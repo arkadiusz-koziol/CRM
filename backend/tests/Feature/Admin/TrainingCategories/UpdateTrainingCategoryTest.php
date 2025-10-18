d7aab27155d44c8b08929bf5ad92f5f53adc4dbc
akoziol <arkadiuszkoziol2@gmail.com>
2025-10-18 21:09:28 +0200

    refactor: update categoryId in TrainingServiceTest to use a static value

--- backend/tests/Feature/Admin/TrainingCategories/UpdateTrainingCategoryTest.php
+++ backend/tests/Feature/Admin/TrainingCategories/UpdateTrainingCategoryTest.php
@@ -19,6 +19,7 @@ final class UpdateTrainingCategoryTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->seed(\Database\Seeders\PermissionSeeder::class);
         $this->admin = User::factory()->create();
         $this->admin->givePermissionTo('training.category.update');
     }
