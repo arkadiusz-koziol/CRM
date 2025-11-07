720537da6285e50976d5a9a17c385f0ed5f19f11
akoziol <arkadiuszkoziol2@gmail.com>
2025-11-07 20:59:33 +0100

    test(docs): add DocsPolicyProbe (isDocsPolicyHealthy returns true)

---
+++ backend/app/Domain/Crm/DocsPolicyProbe.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Crm;
+
+/**
+ * Probe class for testing documentation policy enforcement
+ */
+class DocsPolicyProbe
+{
+    /**
+     * Check if documentation policy enforcement is healthy
+     *
+     * @return bool
+     */
+    public function isDocsPolicyHealthy(): bool
+    {
+        return true;
+    }
+}
